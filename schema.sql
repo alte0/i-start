73327f865058e0774e204cee5e0f4c1ba6c42d70
Максим <makc-dmv@yandex.ru>
2020-10-09 17:00:45 +0500

    rename bd

--- schema.sql
+++ schema.sql
@@ -1,26 +1,7 @@
--- CREATE DATABASE `tasks-db`
---   DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
---
--- USE `tasks-db`;
---
--- CREATE TABLE `users` (
---   `user_id` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
---   `user_login` varchar(20) NOT NULL,
---   `user_password` varchar(60) NOT NULL,
---   `user_name` varchar(20) NOT NULL,
---   `user_surname` varchar(20) NOT NULL,
---   `user_patronymic` varchar(20) NOT NULL,
---   `user_date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
--- )
---   ENGINE=InnoDB
---   DEFAULT CHARSET=utf8
---   COMMENT = 'Таблица пользователей';
-
-
 CREATE DATABASE IF NOT EXISTS `feedback-bd`
   DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-USE `feedback-bd`;
+USE `feedback_bd`;
 
 CREATE TABLE IF NOT EXISTS `feedback` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
